a3cac888871d8286b88c168dbacd694c8e22d7b1
Matheus Vieira <38119742+vssmatheus@users.noreply.github.com>
2021-04-14 01:55:38 -0300

    estrutura pronta

---
+++ src/landing/Landing.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './styles.css';
+
+export default function Landing (){
+   
+    return(
+        <div className="body">
+            <h1>Teste</h1>
+        </div>        
+    );
+}
