bc0c90146c1d9671ecaaf6683b8d7dd0c81fc0ff
Matheus Vieira <38119742+vssmatheus@users.noreply.github.com>
2021-04-19 23:22:29 -0300

    de acordo com o layout

--- src/landing/Landing.js
+++ src/landing/Landing.js
@@ -3,6 +3,7 @@ import './styles.css';
 import logo from "../assets/id_visual/logo.svg";
 import eye from "../assets/icons/icon-eye.svg";
 import eyeSlash from "../assets/icons/icon-eye-show.svg";
+import detail from "../assets/images/detail.svg";
 import {Link} from "react-router-dom";
 import $ from 'jquery';
 
@@ -21,27 +22,28 @@ export default function Landing (){
             document.getElementById('icon_eye').style.display = 'block';
         }
     }
-
+    
     useEffect(() =>{
+       
         /* HAMBURGUER MENU ANIMATION */
         setTimeout(() => {
             const $button = $('#burger');
             const $navLinkWrapper = $('.nav__link-wrapper');
-    
+            
             $button.on('click', function(e) {
                 e.preventDefault();
                 e.stopPropagation();
                 $button.find('.nav__burger-inner').toggleClass('active');    
                 $navLinkWrapper.toggleClass('expanded');
-            });   
-        });
-    }, []);
+            });
+        });        
+    },[]);
 
     return(
         <div className="body container-fluid inicialsite initial-container">
             <div>
                 {/* NAVBAR */}
-                <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+                <nav id="navbar" className="navbar navbar-expand-lg navbar-light fixed-top">
                     <div className="container-fluid container">
                         <button id="burger" className="nav__burger-button">
                             <div className="nav__burger">
@@ -96,6 +98,50 @@ export default function Landing (){
                     </div>
                 </div>
             </div>
+            {/* CONTENT */}
+            <section className="content__center">
+                <div className="container">
+                    <div className="row align-items-center">
+                        <div className="col-12 col-md-5 col-xs-12 datail">
+                            <img src={detail} alt="detail" className="detail__image"/>
+                        </div>
+                        <div className="col-12 col-md-7 col-xs-12" style={{height: "580px"}}>
+                            <div className="form__container">
+                                <form action="">
+                                    <h3 className="form__title">
+                                        Find your <br/> <span className="form__subtitle">BEST TEACHER</span>
+                                    </h3>
+                                    <br/>
+                                    <p className="text__form">
+                                    Whether you are a student trying to find your ideal private language teachers/tutors or a teacher trying to find great students for your customised private lessons!
+                                    </p>
+                                    <br/>
+                                    <input placeholder="Type here what are you looking for" type="text" className="input__login" style={{color: "#ffffff"}}/>
+                                    <div className="row" style={{margin: "20px 0"}}>
+                                        <div className="col-12 col-md-7 col-xs-12" style={{ padding: "0"}}>
+                                            <div className="btn__perfil-container">
+                                                <label className="btn__perfil">I´M TEACHER
+                                                    <input id="check" type="radio" checked="checked" name="radio"/>
+                                                    <span class="checkmark"></span>
+                                                </label>
+                                                <label class="btn__perfil">I´M STUDENT
+                                                    <input id="check" type="radio" name="radio" autoFocus/>
+                                                    <span class="checkmark"></span>
+                                                </label> 
+                                            </div>                                                                                           
+                                        </div>
+                                        <div className="col-12 col-md-5 col-xs-12" style={{ padding: "0"}}>
+                                            <button type="button" className="btn__search">SEARCH</button>
+                                        </div>
+                                    </div>
+
+                                </form>
+                            </div>
+                            
+                        </div>
+                    </div>
+                </div>
+            </section>
         </div>        
     );
 }
